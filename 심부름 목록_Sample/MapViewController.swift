c49c61b54546deb313057188b847b31252889f9b
moon <mos5656@naver.com>
2017-08-16 15:19:42 +0900

    맵 라벨 수

--- 심부름 목록_Sample/MapViewController.swift
+++ 심부름 목록_Sample/MapViewController.swift
@@ -41,19 +41,19 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         myMap.showsUserLocation = true
         
         //심부름1
-        setAnnotation(latitude: 37.5863660, longitude: 127.0289090, delta: 1, title: "안암역", subtitle: "서울 성북구 안암동")
+        setAnnotation(latitude: 37.5863660, longitude: 127.0289090, delta: 1, title: "안암역", subtitle: "서울 성북구 안암동", task: "별 일:", detail: "현금 만원만 인출해주기")
         
         //심부름2
-        setAnnotation(latitude: 37.5926340, longitude: 127.0249460, delta: 1, title: "화정체육관", subtitle: "서울 성북구 안암동")
+        setAnnotation(latitude: 37.5926340, longitude: 127.0249460, delta: 1, title: "화정체육관", subtitle: "서울 성북구 안암동", task: "별 일:", detail: "피트니스 대신 연장해주기")
         
         //심부름3
-        setAnnotation(latitude: 37.5830406, longitude: 127.02917379999997, delta: 1, title: "버거킹", subtitle: "서울 성북구 안암동 안암로 73")
+        setAnnotation(latitude: 37.5830406, longitude: 127.02917379999997, delta: 1, title: "버거킹", subtitle: "서울 성북구 안암동 안암로 73", task: "별 일:", detail: "버거킹에서 콰트로치즈와퍼 포장")
         //심부름4
-        setAnnotation(latitude: 37.5886719, longitude: 127.02938059999997, delta: 1, title: "아눅", subtitle: "서울 성북구 안암동 15-82")
+        setAnnotation(latitude: 37.5886719, longitude: 127.02938059999997, delta: 1, title: "아눅", subtitle: "서울 성북구 안암동 15-82", task: "별 일:", detail: "아눅에서 샌드위치 주문")
         //심부름5
-        setAnnotation(latitude: 37.5890055, longitude: 127.02936350000004, delta: 1, title: "승가마트", subtitle: "서울 성북구 안암동 12-81")
+        setAnnotation(latitude: 37.5890055, longitude: 127.02936350000004, delta: 1, title: "승가마트", subtitle: "서울 성북구 안암동 12-81", task: "별 일:", detail: "택배 대리 수령")
         //심부름6
-        setAnnotation(latitude: 37.5892274, longitude: 127.02849170000002, delta: 1, title: "개운사", subtitle: "서울 성북구 안암동 157-3")
+        setAnnotation(latitude: 37.5892274, longitude: 127.02849170000002, delta: 1, title: "개운사", subtitle: "서울 성북구 안암동 157-3", task: "별 일:", detail: "개운사에서 불공드리기")
 
     }
     
@@ -71,17 +71,16 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         return pLocation
     }
     
-    func setAnnotation(latitude latitudeValue: CLLocationDegrees, longitude longitudeValue : CLLocationDegrees, delta span : Double, title strTitle: String, subtitle strsubtitle:String){
+    func setAnnotation(latitude latitudeValue: CLLocationDegrees, longitude longitudeValue : CLLocationDegrees, delta span : Double, title strTitle: String, subtitle strsubtitle:String, task strTask: String, detail strDetail: String){
         
         let annotation = CustomPointAnnotation() //MKPointAnnotation() 클래스 새로 만들어서 대체함
         annotation.coordinate = goLocation(latitude: latitudeValue, longitude: longitudeValue, delta: span) //golocation 왜 안쓰이지?
-        annotation.title = strTitle
-        annotation.subtitle = strsubtitle
         
-        //핀 셀렉될 때 밑에 표시해야함
-        //self.lblLocationInfo1.text = strtasktitle
-        //self.lblLocationInfo2.text = strtaskdetail
-
+        //핀 제목과 디테일
+        annotation.title = strTask//strTitle
+        annotation.subtitle = strDetail//strsubtitle
+        annotation.locationTitle = strTitle //지명
+        annotation.locationDetail = strsubtitle //상세위치
         myMap.addAnnotation(annotation)
     }
     
@@ -110,7 +109,16 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     
     //핀이 선택되었을 때 하단의 라벨 : 심부름 이름과 내용 변경 - 나중에
-    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        print("Annotation selected")
+        
+        if let annotation = view.annotation as? CustomPointAnnotation {
+            print("Your annotation title: \(annotation.title)");
+            
+            lblLocationInfo1.text = annotation.locationTitle! //strtasktitle
+            lblLocationInfo2.text = annotation.locationDetail! //strtaskdetail
+        }
         
 //        if let annotation = view.annotation as? CustomPointAnnotation {
 //            self.lblLocationInfo1.text = tasktitle
@@ -152,4 +160,6 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
 //커스텀 핀을 위한 클래스 하나 더 추가
 class CustomPointAnnotation: MKPointAnnotation {
     var imageName: String! //이 변수 필요
+    var locationTitle: String!
+    var locationDetail: String!
 }
